98d2dfa3cffe8d004a747b14e5bd5fdb52988bcf
Aaditya <codewithaadi31@gmail.com>
2024-05-01 17:19:04 +0530

    Project Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
 void main() {
-  runApp();
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 60, 9, 202),
+          Color.fromARGB(255, 23, 13, 41),
+        ),
+      ),
+    ),
+  );
 }
